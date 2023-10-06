55169a4e5b13cb3829aef0024c55acce3e62b2e4
Miguel-A17 <msalazar17@alumnos.uaq.mx>
2023-10-06 16:47:17 -0600

    Primer commit: Archivos iniciales

---
+++ pascall.h
@@ -0,0 +1,16 @@
+#pragma once
+
+//constructor
+
+// Primer branch
+// Primer checkout
+class pascall
+{
+	int ** triangulo;
+	int N;
+public:
+	pascall(int n);
+	void crearTrian();
+	void muestraT();
+};
+
---
+++ trian_Pascal.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include"pascall.h"
+#define N 4
+
+
+using namespace std;
+
+
+
+int main()
+{
+	
+	
+	pascall t1(5);
+	t1.crearTrian();
+	t1.muestraT();
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	int ** triangulo;
+	triangulo = new int* [N];
+
+	for (int i = 0; i < N; i++) {
+		triangulo[i] = new int[i + 1];
+		cout << endl;
+		for (int j = 0;j <= i;j++) {	
+			triangulo[i][j] = 1;
+			if (j != 0 && j != 1) {
+				triangulo[i][j] = triangulo[i-1][j-1] + triangulo[i-1][j];
+			}
+			cout << " " << " i[" << i << "] j[" << j << "]   " << triangulo[i][j];
+			
+		}
+	}
+	*/
+	
+}
